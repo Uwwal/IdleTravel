4901802942488d31e649d29846edc0214284ed8f
Calvariaa <1764203060@qq.com>
2021-07-21 16:54:52 +0800

    add Desert area

--- app/src/main/java/com/example/idletravel/customItem/items/ItemExample.kt
+++ app/src/main/java/com/example/idletravel/customItem/items/ItemExample.kt
@@ -4,3 +4,4 @@ import com.example.idletravel.customItem.CustomItem
 
 val garbageItem = CustomItem(1,"垃圾","垃圾")
 val softTwigsItem = CustomItem(2,"树枝","树枝")
+val cactusItem = CustomItem(3,"仙人掌","仙人掌")
