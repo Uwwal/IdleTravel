cbb2a87d69c82ebc46a1b474186e29a79f700793
Uwwal <3476365499@qq.com>
2021-07-24 01:46:36 +0800

    roll back + 注释

--- app/src/main/java/com/example/idletravel/player/Player.kt
+++ app/src/main/java/com/example/idletravel/player/Player.kt
@@ -9,6 +9,8 @@ class Player(
     var sex: String,
     var status: MutableList<Double> = mutableListOf(0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00),
     // 力量 体质 灵巧 感知 学识 意志 魔力 魅力
+    // 这里不要把list改成枚举类 血的教训
+    // 如果改成枚举类, 则还需要一个list来保证随机奖励属性功能正常运行(位置在Travel.kt)
     var finishMapTime: HashMap<Area, Int> = hashMapOf(
         grasslandArea to 0
     )
